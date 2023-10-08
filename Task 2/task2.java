3ded1e7c1d9302f57333967467ddc1ec826846dd
uddhavrawal <147224883+uddhavrawal@users.noreply.github.com>
2023-10-08 10:45:53 +0530

    Create task2.java

---
+++ Task 2/task2.java
@@ -0,0 +1,47 @@
+ 
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input: Take marks obtained in each subject
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+        int totalMarks = 0;
+
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + i + ": ");
+            int marks = scanner.nextInt();
+            totalMarks += marks;
+        }
+
+        // Calculate Total Marks
+        System.out.println("Total Marks: " + totalMarks);
+
+        // Calculate Average Percentage
+        double averagePercentage = (double) totalMarks / numSubjects;
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+
+        // Grade Calculation
+        String grade;
+        if (averagePercentage >= 90) {
+            grade = "A+";
+        } else if (averagePercentage >= 80) {
+            grade = "A";
+        } else if (averagePercentage >= 70) {
+            grade = "B";
+        } else if (averagePercentage >= 60) {
+            grade = "C";
+        } else if (averagePercentage >= 50) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+
+        // Display Results
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
